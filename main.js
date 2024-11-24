9061624a240af58383fcb45a7b9bec374b276bd2
MicaPersonal <miqa.orellano@gmail.com>
2024-11-24 04:32:36 -0300

    Inicio de proyecto

---
+++ main.js
@@ -0,0 +1,68 @@
+// Trabajo Práctico Grupal 2
+
+// -------------------- Ejercicio 1------------------------
+function ej1 () {
+
+}
+
+/*  Explicacion de ejercicio 1: */
+
+// -------------------- Ejercicio 2------------------------
+function ej2 (){
+   
+}
+
+/* Explicacion de ejercicio 2:  */
+
+// -------------------- Ejercicio 3------------------------
+function ej3 (){
+}
+
+/* Explicacion de ejercicio 3: */
+
+// -------------------- Ejercicio 4------------------------
+function ej4 (){
+}
+
+/* Explicacion de ejercicio 4:  */
+
+// -------------------- Ejercicio 5------------------------
+function ej5 (){
+}
+
+/* Explicacion de ejercicio 5: */
+
+// -------------------- Ejercicio 6------------------------
+function ej6 (){
+}
+
+/* Explicacion de ejercicio 6: */
+
+
+// -------------------- Ejercicio 7------------------------
+function ej7 (){
+}
+
+/* Explicacion de ejercicio 7: */
+
+// -------------------- Pruebas de ejercicios------------------------
+//Ejercicio 1
+console.log("-------Prueba ejercicio 1-------")
+
+//Ejercicio 2
+console.log("-------Prueba ejercicio 2-------")
+
+//Ejercicio 3
+console.log("-------Prueba ejercicio 3-------")
+
+//Ejercicio 4
+console.log("-------Prueba ejercicio 4-------")
+
+//Ejercicio 5
+console.log("-------Prueba ejercicio 5-------") 
+
+//Ejercicio 6
+console.log("-------Prueba ejercicio 5-------") 
+
+//Ejercicio 7
+console.log("-------Prueba ejercicio 5-------") 
